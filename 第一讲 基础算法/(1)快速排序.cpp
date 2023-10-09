afea8dba9d6dbb5e6a4550fa0f0b1f905a30bd20
LMH-ironman <970259067@qq.com>
2023-10-09 21:02:31 +0800

    sort check return this game 10.9

--- 第一讲 基础算法/(1)快速排序.cpp
+++ 第一讲 基础算法/(1)快速排序.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-const int N = 1e6 + 10;
+const int N = 100010;
 
 int n;
 int q[N];
@@ -11,32 +12,30 @@ void quick_sort(int q[], int l, int r)
 {
     if (l >= r)
         return;
-
     int x = q[(l + r) / 2], i = l - 1, j = r + 1;
     while (i < j)
     {
         do
+        {
             i++;
-        while (q[i] < x);
+        } while (q[i] < x);
         do
+        {
             j--;
-        while (q[j] > x);
+        } while (q[j] > x);
         if (i < j)
             swap(q[i], q[j]);
     }
     quick_sort(q, l, j);
     quick_sort(q, j + 1, r);
 }
-
 int main()
 {
-    scanf("%d", &n);
+    scanf("%d\n", &n);
     for (int i = 0; i < n; i++)
         scanf("%d", &q[i]);
-
     quick_sort(q, 0, n - 1);
 
     for (int i = 0; i < n; i++)
         printf("%d ", q[i]);
-    return 0;
 }
--- 第一讲 基础算法/(2)归并排序.cpp
+++ 第一讲 基础算法/(2)归并排序.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-
 using namespace std;
 
-const int N = 1000010;
+const int N = 100010;
 
 int n;
 int q[N], tmp[N];
@@ -11,10 +10,9 @@ void merge_sort(int q[], int l, int r)
 {
     if (l >= r)
         return;
-
-    int mid = l + r >> 1;
-    merge_sort(q, l, mid), merge_sort(q, mid + 1, r);
-
+    int mid = (l + r) / 2;
+    merge_sort(q, l, mid);
+    merge_sort(q, mid + 1, r);
     int k = 0, i = l, j = mid + 1;
     while (i <= mid && j <= r)
     {
@@ -27,23 +25,15 @@ void merge_sort(int q[], int l, int r)
         tmp[k++] = q[i++];
     while (j <= r)
         tmp[k++] = q[j++];
-
-    for (i = l, j = 0; i <= r; i++, j++)
-    {
+    for (int i = l, j = 0; i <= r; i++, j++)
         q[i] = tmp[j];
-    }
 }
-
 int main()
 {
-    scanf("%d", &n);
+    scanf("%d\n", &n);
     for (int i = 0; i < n; i++)
         scanf("%d", &q[i]);
-
     merge_sort(q, 0, n - 1);
-
     for (int i = 0; i < n; i++)
         printf("%d ", q[i]);
-
-    return 0;
 }
