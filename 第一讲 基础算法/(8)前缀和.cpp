846fa5e619251ce2a8bc442422a51b60197f5667
LMH-ironman <970259067@qq.com>
2023-10-17 14:39:27 +0800

    basic algorithm 10.17

---
+++ 第一讲 基础算法/(8)前缀和.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 100001;
+
+int n, m;
+int a[N], s[N];
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
+    for (int i = 1; i <= n; i++)
+        s[i] = s[i - 1] + a[i];
+    while (m--)
+    {
+        int l, r;
+        scanf("%d%d\n", &l, &r);
+        printf("%d\n", s[r] - s[l - 1]);
+    }
+    return 0;
+}
